0683856e3030f913626b7f9bf367ec5710996479
dmeraz <darin.meraz@gmail.com>
2016-09-16 16:53:29 -0400

    added fields and properties

--- VirtualPet.cs
+++ VirtualPet.cs
@@ -8,5 +8,41 @@ namespace MerazVirtualPet
 {
     class VirtualPet
     {
+        //Fields
+
+        private string name;
+        private int hungerScore;
+        private int energyScore;
+        private int wasteScore;
+        private string treat;
+        private string favToy;
+        
+        //Properties
+        public VirtualPet (string name)
+        {
+            this.name = name;
+        }
+
+        public VirtualPet (int hungerScore, int energyScore, int wasteScore)
+        {
+            this.hungerScore = hungerScore;
+            this.energyScore = energyScore;
+            this.energyScore = wasteScore;
+        }
+
+        public VirtualPet (string name, string favToy)
+        {
+            this.name = name;
+            this.favToy = favToy;
+        }
+
+        //Methods
+        public string GetName()
+        {
+            return name;
+        }
+
+        public 
+
     }
 }
