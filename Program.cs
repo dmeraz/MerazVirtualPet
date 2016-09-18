00195b32906e0645bd858e42cffa4ab4a45dde6d
dmeraz <darin.meraz@gmail.com>
2016-09-17 22:36:54 -0400

    added scoreboard

--- Program.cs
+++ Program.cs
@@ -10,12 +10,27 @@ namespace MerazVirtualPet
     {
         static void Main(string[] args)
         {
+            //Introduce user to their new pet and ask for a name
+
             Console.WriteLine("You have a new pet hedgehog! Give your hedgie a name.\n");
+
             Console.WriteLine("  .|||||||||.");
             Console.WriteLine(" |||||||||||||");
             Console.WriteLine("||||||||||| ' .\\");
             Console.WriteLine("`|||||||||| _, __o\n");
-            Console.ReadLine();
+
+            Console.WriteLine("Hunger\t\tEnergy\t\tWaste"); //{ Hunger = 10, Energy = 10, Waste = 10}
+            //VirtualPet.Tick();
+            //Console.WriteLine(Hunger + "\t\t" + Energy + "\t\t" + Waste + "\n");
+
+            VirtualPet name = new VirtualPet(Console.ReadLine()) ;
+
+            //Ask user to pick treat and toy types to reward hedgie
+
+
+
+            //Introduce care points
+
         }
     }
 }
